84bc5ca520d27b342906ef4d70c78d286ca506b4
Laveden <157773978+LavedenC1@users.noreply.github.com>
2024-09-09 11:39:28 -0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,6 @@
 
 wget https://dist.torproject.org/torbrowser/13.5.3/tor-browser-linux-x86_64-13.5.3.tar.xz
 tar -xf tor-browser-linux-x86_64-13.5.3.tar.xz
-cd tor-browser-linux-x86_64-13.5.3
+cd tor-browser
 ./start-tor-browser.desktop --register-app
+./start-tor-browser.desktop
